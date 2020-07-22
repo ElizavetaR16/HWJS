07131455ea65e0fe0b38a847d5c59a20572cc436
ElizavetaR16 <62614466+ElizavetaR16@users.noreply.github.com>
2020-07-22 14:20:46 +0300

    Add files via upload

--- ToDo list/jva.js
+++ ToDo list/jva.js
@@ -1,27 +1,73 @@
-﻿var list = document.querySelector('ul');
-    list.addEventListener('click', function (ev) {
-        if(ev.target.tagName === "LI") {
-        ev.target.classList.toggle('checked');
-        } else if(ev.target.tagName === "SPAN") {
-        var div = ev.target.parentNode;
-        div.remove();
+﻿var input = document.querySelector("input[type='text']");
+var ul = document.querySelector("ul");
+var container = document.querySelector("div");
+var lists = document.querySelectorAll("li");
+var spans = document.getElementsByTagName("span");
+var pencil = document.querySelector("#pencil");
+var saveBtn = document.querySelector(".save");
+var clearBtn = document.querySelector(".clear");
+var lists = document.querySelectorAll("li");
+var tipsBtn = document.querySelector(".tipBtn");
+var closeBtn = document.querySelector(".closebtn");
+var overlay = document.getElementById("overlay");
+
+pencil.addEventListener('click', function(){
+    input.classList.toggle ("display");
+
+});
+function DeleteTodo() {
+    for (let span of spans){
+        span.addEventListener("click", function (){
+            span.parentElement.remove();
+        });
+    }
+};
+tipsBtn.addEventListener("click", function(){
+overlay.style.height="100%";
+});
+closeBtn.addEventListener("click", function(){
+    overlay.style.height="0";
+});
+ul.addEventListener('click', function (e) {
+    if(e.target.tagName === "LI") {
+       e.target.classList.toggle('checked');
+    } else if(e.target.tagName === "SPAN") {
+       var div = e.target.parentNode;
+       div.remove();
+    }
+}, 
+false);
+input.addEventListener ("keypress", function(key){
+    if(key.which ===13){
+        var li = document.createElement ("li");
+        var spanElement = document.createElement ("span");
+        var icon = document.createElement ("i");
+
+        var newtodo = this.value;
+        this.value ="";
+        if(newtodo == "") {
+            alert("Вы ничего не написали!");
+        } else{
+        icon.classList.add ('fas', 'fa-trash-alt');
+        spanElement.append (icon);
+        ul.appendChild(li).append(spanElement, newtodo);
+        DeleteTodo();
         }
-    }, 
-    false);
-function newElement() {
-    var li = document.createElement('li');
-    var inputVal = document.getElementById('toDoEl').value;
-    var t = document.createTextNode(inputVal);
-    li.appendChild(t);
-        if(inputVal == "") {
-        alert("Ты ничего не ввёл!");
-        } else {
-        document.getElementById('list').appendChild(li);
-        }
-    document.getElementById('toDoEl').value = "";
-    var span = document.createElement('SPAN');
-    var txt = document.createTextNode("X");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    }
+});
+clearBtn.addEventListener('click', function () {
+    ul.innerHTML= "";
+    localStorage.removeItem("todoList", ul.innerHTML);
+ });
+ saveBtn.addEventListener('click', function () {
+    localStorage.setItem ("todoList", ul.innerHTML);
+ });
+
+ function loadTodos() {
+    const data = localStorage.getItem("todoList");
+    if (data) {
+        ul.innerHTML = data;
+    }
+    DeleteTodo();
 }
+loadTodos();
